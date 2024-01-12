9186c9d61943266a8bafba09cd1b49aef39d2561
Saqib <sjmugha@gmail.com>
2024-01-12 15:41:13 +0500

    Update weather.vue

--- src/views/weather.vue
+++ src/views/weather.vue
@@ -3,197 +3,202 @@
 
   <div class="py-10 lg:py-5 text-white">
     <!-- inner div as a main container of weather app -->
-    <div
-      class="w-[20rem] md:w-[35rem] lg:w-[50rem] overflow-hidden rounded-xl flex flex-col lg:flex-row flex-wrap gap-4 items-center lg:justify-center mx-auto lg:bg-yellow-900 lg:bg-opacity-50 lg:py-3 bg-black bg-opacity-45 py-2"
-      v-if="state.loading == false"
-    >
+
+    <transition name="mainFade">
       <div
-        class="leftContainer lg:w-[40%] rounded-xl overflow-hidden flex flex-col justify-center items-center p-2 space-y-2 bg-gradient-to-r via-yellow-600 to-yellow-900 pt-5"
+        class="w-[20rem] md:w-[35rem] lg:w-[50rem] overflow-hidden rounded-xl flex flex-col lg:flex-row flex-wrap gap-4 items-center lg:justify-center mx-auto lg:bg-yellow-900 lg:bg-opacity-50 lg:py-3 bg-black bg-opacity-45 py-2"
+        v-if="state.loading == false"
       >
-        <div class="w-full flex flex-col gap-5">
-          <input
-            name="text"
-            type="text"
-            class="outline-dotted outline-1 outline-orange-400 rounded-2xl pl-5 text-xs py-[3px] bg-black w-full"
-            placeholder="search for place "
-            @keypress.enter="searchForSpecificPlace"
-            v-model="state.searchedCountry"
-          />
-          <transition name="fade">
-            <div
-              v-if="state.errorMsgToUser != ''"
-              class="bg-red-600 text-center w-56 absolute mt-7 ml-11 text-lg font-bold"
-            >
-              {{ state.errorMsgToUser }}
-            </div>
-          </transition>
-        </div>
-        <div class="flex space-x-2">
-          <div
-            class="cursor-pointer hover:text-amber-900 transition delay-75 ease-in-out text-sm font-semibold"
-            @click="getLahoreWeather"
-          >
-            Karachi
-          </div>
-          <div
-            class="cursor-pointer hover:text-amber-900 transition delay-75 ease-in-out text-sm font-semibold"
-            @click="getIslamabadWeather"
-          >
-            Islamabad
-          </div>
-          <div
-            class="cursor-pointer hover:text-amber-900 transition delay-75 ease-in-out text-sm font-semibold"
-            @click="getMyLocationWeather"
-          >
-            My location
-          </div>
-        </div>
         <div
-          class="innerLeftContainers flex flex-col justify-center items-center space-y-5"
+          class="leftContainer lg:w-[40%] rounded-xl overflow-hidden flex flex-col justify-center items-center p-2 space-y-2 bg-gradient-to-r via-yellow-600 to-yellow-900 pt-5"
         >
+          <div class="w-full flex flex-col gap-5">
+            <input
+              name="text"
+              type="text"
+              class="outline-dotted outline-1 outline-orange-400 rounded-2xl pl-5 text-xs py-[3px] bg-black w-full"
+              placeholder="search for place "
+              @keypress.enter="searchForSpecificPlace"
+              v-model="state.searchedCountry"
+            />
+            <transition name="fade">
+              <div
+                v-if="state.errorMsgToUser != ''"
+                class="bg-red-600 text-center w-56 absolute mt-7 ml-11 text-lg font-bold"
+              >
+                {{ state.errorMsgToUser }}
+              </div>
+            </transition>
+          </div>
+          <div class="flex space-x-2">
+            <div
+              class="cursor-pointer hover:text-amber-900 transition delay-75 ease-in-out text-sm font-semibold"
+              @click="getLahoreWeather"
+            >
+              Karachi
+            </div>
+            <div
+              class="cursor-pointer hover:text-amber-900 transition delay-75 ease-in-out text-sm font-semibold"
+              @click="getIslamabadWeather"
+            >
+              Islamabad
+            </div>
+            <div
+              class="cursor-pointer hover:text-amber-900 transition delay-75 ease-in-out text-sm font-semibold"
+              @click="getMyLocationWeather"
+            >
+              My location
+            </div>
+          </div>
           <div
-            class="upper flex flex-col justify-center items-center p-2 rounded-2xl space-y-4"
+            class="innerLeftContainers flex flex-col justify-center items-center space-y-5"
           >
-            <div v-if="state.currentTemperature == ''">Loading...</div>
-            <div v-else class="mainShow">
-              <div class="text-xl font-semibold text-center">
-                {{ state.currentLocationName }}
-              </div>
-              <div class="text-4xl font-bold text-center">
-                {{ state.currentTemperature.toFixed() }}
-                {{ state.currentTemperatureUnit }}
-              </div>
-              <!-- <div>
+            <div
+              class="upper flex flex-col justify-center items-center p-2 rounded-2xl space-y-4"
+            >
+              <div v-if="state.currentTemperature == ''">Loading...</div>
+              <div v-else class="mainShow">
+                <div class="text-xl font-semibold text-center">
+                  {{ state.currentLocationName }}
+                </div>
+                <div class="text-4xl font-bold text-center">
+                  {{ state.currentTemperature.toFixed() }}
+                  {{ state.currentTemperatureUnit }}
+                </div>
+                <!-- <div>
                 <img :src="state.link" alt="" class="mx-auto" />
               </div> -->
-            </div>
-            <div class="mainShow text-xl">
-              {{ state.currentState }}
-            </div>
-            <div class="mainShow text-center text-xs text-slate-300">
-              Today there is {{ state.currentDescription }} all around
-            </div>
-          </div>
-          <div class="lower grid grid-cols-2 gap-4 p-2 w-[18rem]">
-            <div
-              class="p-2 flex flex-col bg-yellow-900 bg-opacity-50 rounded-xl"
-            >
-              <div class="text-xl font-semibold">Feels like</div>
-              <div class="font-semibold mt-2">
-                {{ state.currentFeelsLike }}
+              </div>
+              <div class="mainShow text-xl">
+                {{ state.currentState }}
+              </div>
+              <div class="mainShow text-center text-xs text-slate-300">
+                Today there is {{ state.currentDescription }} all around
               </div>
             </div>
-            <div class="p-2 flex bg-yellow-900 bg-opacity-50 rounded-xl">
-              <!-- forwind -->
-              <div>
-                <div class="text-xl font-semibold">Wind</div>
-                <div class="flex gap-3">
-                  <div class="grid place-items-center">
-                    <div class="font-semibold">
-                      {{ state.currentWindSpeed }}
-                    </div>
-                  </div>
-                  <div class="text-xs mt-2 text-slate-300">
-                    <div>Kmph</div>
-                    <div>wind</div>
-                  </div>
-                </div>
-                <div class="flex gap-3">
-                  <div class="grid place-items-center">
-                    <div class="font-semibold">
-                      {{ state.currentGust }}
-                    </div>
-                  </div>
-                  <div class="text-xs mt-2 text-slate-300">
-                    <div>Kmph</div>
-                    <div>gust</div>
-                  </div>
-                </div>
-              </div>
-              <!-- <div class="grid place-items-center">direction logo</div> -->
-            </div>
-            <div
-              class="p-2 flex flex-col bg-yellow-900 bg-opacity-50 rounded-xl"
-            >
-              <div class="text-xl font-semibold">Visiblity</div>
-              <div class="font-semibold mt-2">
-                {{ state.currentVisibility / 1000 }} <span>Km</span>
-              </div>
-            </div>
-            <div
-              class="p-2 flex flex-col bg-yellow-900 bg-opacity-50 rounded-xl"
-            >
-              <div class="text-xl font-semibold">Humidity</div>
-              <div class="font-semibold mt-2">{{ state.currenHumdity }}%</div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div
-        class="rightContainer w-[18rem] md:w-[30rem] lg:w-[55%] rounded-xl flex flex-col p-2 justify-center items-center space-y-2 overflow-hidden"
-      >
-        <div
-          class="flex flex-col w-full rounded-xl gap-2 p-2 bg-amber-900 bg-opacity-70"
-        >
-          <div class="flex text-sm text-yellow-500">Hourly Forcast</div>
-          <div class="border-t-2 border-t-yellow-600"></div>
-          <div class="flex space-x-1 overflow-y-auto p-1 m-1">
-            <div v-for="(forecast, index) in getHourlyForecast()" :key="index">
+            <div class="lower grid grid-cols-2 gap-4 p-2 w-[18rem]">
               <div
-                class="px-2 mb-2 w-20 rounded-lg pt-2 pl-3"
-                :class="{
-                  'shadow-[0_0_5px_2px] shadow-yellow-300': index == 0,
-                }"
+                class="p-2 flex flex-col bg-yellow-900 bg-opacity-50 rounded-xl"
               >
-                <div class="text-[9px]">
-                  {{ new Date(forecast.dt_txt).toLocaleTimeString() }}
+                <div class="text-xl font-semibold">Feels like</div>
+                <div class="font-semibold mt-2">
+                  {{ state.currentFeelsLike }}
                 </div>
-                <div class="font-semibold">
-                  {{ forecast.main.temp.toFixed() }}°C
+              </div>
+              <div class="p-2 flex bg-yellow-900 bg-opacity-50 rounded-xl">
+                <!-- forwind -->
+                <div>
+                  <div class="text-xl font-semibold">Wind</div>
+                  <div class="flex gap-3">
+                    <div class="grid place-items-center">
+                      <div class="font-semibold">
+                        {{ state.currentWindSpeed }}
+                      </div>
+                    </div>
+                    <div class="text-xs mt-2 text-slate-300">
+                      <div>Kmph</div>
+                      <div>wind</div>
+                    </div>
+                  </div>
+                  <div class="flex gap-3">
+                    <div class="grid place-items-center">
+                      <div class="font-semibold">
+                        {{ state.currentGust }}
+                      </div>
+                    </div>
+                    <div class="text-xs mt-2 text-slate-300">
+                      <div>Kmph</div>
+                      <div>gust</div>
+                    </div>
+                  </div>
                 </div>
-                <img
-                  :src="getWeatherIconUrl(forecast.weather[0].icon)"
-                  alt=""
-                />
+                <!-- <div class="grid place-items-center">direction logo</div> -->
+              </div>
+              <div
+                class="p-2 flex flex-col bg-yellow-900 bg-opacity-50 rounded-xl"
+              >
+                <div class="text-xl font-semibold">Visiblity</div>
+                <div class="font-semibold mt-2">
+                  {{ state.currentVisibility / 1000 }} <span>Km</span>
+                </div>
+              </div>
+              <div
+                class="p-2 flex flex-col bg-yellow-900 bg-opacity-50 rounded-xl"
+              >
+                <div class="text-xl font-semibold">Humidity</div>
+                <div class="font-semibold mt-2">{{ state.currenHumdity }}%</div>
               </div>
             </div>
           </div>
         </div>
         <div
-          class="flex flex-col w-full rounded-xl gap-2 p-2 bg-amber-900 bg-opacity-70"
+          class="rightContainer w-[18rem] md:w-[30rem] lg:w-[55%] rounded-xl flex flex-col p-2 justify-center items-center space-y-2 overflow-hidden"
         >
-          <div class="text-sm text-yellow-500">5 days Forcast</div>
-          <div class="border-t-2 border-t-yellow-600"></div>
-          <div class="flex overflow-y-auto space-x-1 p-1 m-1">
-            <div
-              v-for="(forecast, index) in getNext5DaysForecast()"
-              :key="index"
-            >
+          <div
+            class="flex flex-col w-full rounded-xl gap-2 p-2 bg-amber-900 bg-opacity-70"
+          >
+            <div class="flex text-sm text-yellow-500">Hourly Forcast</div>
+            <div class="border-t-2 border-t-yellow-600"></div>
+            <div class="flex space-x-1 overflow-y-auto p-1 m-1">
               <div
-                class="px-2 mb-2 w-20 rounded-lg pt-2 pl-3 space-y-1"
-                :class="{
-                  'shadow-[0_0_5px_2px] shadow-yellow-300': index == 0,
-                }"
+                v-for="(forecast, index) in getHourlyForecast()"
+                :key="index"
               >
-                <div v-if="index == 0" class="font-semibold">Today</div>
-                <div v-else class="font-semibold">
-                  {{ getDayName(new Date(forecast.dt_txt)) }}
+                <div
+                  class="px-2 mb-2 w-20 rounded-lg pt-2 pl-3"
+                  :class="{
+                    'shadow-[0_0_5px_2px] shadow-yellow-300': index == 0,
+                  }"
+                >
+                  <div class="text-[9px]">
+                    {{ new Date(forecast.dt_txt).toLocaleTimeString() }}
+                  </div>
+                  <div class="font-semibold">
+                    {{ forecast.main.temp.toFixed() }}°C
+                  </div>
+                  <img
+                    :src="getWeatherIconUrl(forecast.weather[0].icon)"
+                    alt=""
+                  />
                 </div>
-                <div class="text-[9px]">
-                  {{ new Date(forecast.dt_txt).toLocaleDateString() }}
-                </div>
-                <div class="font-semibold">
-                  {{ forecast.main.temp.toFixed() }}°C
-                </div>
-                <img
-                  :src="getWeatherIconUrl(forecast.weather[0].icon)"
-                  alt=""
-                />
               </div>
             </div>
           </div>
-        </div>
-        <!-- <div class="border flex w-full rounded-xl justify-center ">
+          <div
+            class="flex flex-col w-full rounded-xl gap-2 p-2 bg-amber-900 bg-opacity-70"
+          >
+            <div class="text-sm text-yellow-500">5 days Forcast</div>
+            <div class="border-t-2 border-t-yellow-600"></div>
+            <div class="flex overflow-y-auto space-x-1 p-1 m-1">
+              <div
+                v-for="(forecast, index) in getNext5DaysForecast()"
+                :key="index"
+              >
+                <div
+                  class="px-2 mb-2 w-20 rounded-lg pt-2 pl-3 space-y-1"
+                  :class="{
+                    'shadow-[0_0_5px_2px] shadow-yellow-300': index == 0,
+                  }"
+                >
+                  <div v-if="index == 0" class="font-semibold">Today</div>
+                  <div v-else class="font-semibold">
+                    {{ getDayName(new Date(forecast.dt_txt)) }}
+                  </div>
+                  <div class="text-[9px]">
+                    {{ new Date(forecast.dt_txt).toLocaleDateString() }}
+                  </div>
+                  <div class="font-semibold">
+                    {{ forecast.main.temp.toFixed() }}°C
+                  </div>
+                  <img
+                    :src="getWeatherIconUrl(forecast.weather[0].icon)"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- <div class="border flex w-full rounded-xl justify-center ">
           <div class="border p-2 flex flex-col">
             <div>UV Index</div>
             <div>3</div>
@@ -202,9 +207,10 @@
             <div>use sun protection</div>
           </div>
         </div> -->
+        </div>
       </div>
-    </div>
-    <div class="loading-message transition-all" v-else>Loading...</div>
+      <div class="loading-message" v-else>Loading...</div>
+    </transition>
   </div>
 </template>
 
@@ -439,5 +445,23 @@ onMounted(async () => {
 }
 .fade-leave-active {
   transition: all 0.3s ease;
+}
+.mainFade-enter-from {
+  opacity: 0;
+}
+.mainFade-enter-to {
+  opacity: 1;
+}
+.mainFade-enter-active {
+  transition: all 0.3s ease;
+}
+.mainFade-leave-from {
+  opacity: 1;
+}
+.mainFade-leave-to {
+  opacity: 0;
+}
+.mainFade-leave-active {
+  transition: all 0.1s ease;
 }
 </style>
